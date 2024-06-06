1a17c76596a24e06b32f24aea5f0e7a49eefc54f
JulianLoewenherz <juliancollege123@gmail.com>
2024-06-06 16:40:08 -0700

    printing out the datafile

--- linearRegression.py
+++ linearRegression.py
@@ -5,6 +5,11 @@ import seaborn as sns
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("Fishnums.csv")
+df = pd.read_csv("Fish.csv")
 print('Shape of dataset= ', df.shape) # To get no of rows and columns
 df.head(5) # head(n) returns first n records only. Can also use sample(n) for random n records.
+
+df.rename(columns={'Length1':'VerticalLen','Length2':'DiagonalLen','Length3':'CrossLen'},inplace = True) # 'inplace= true' to make change in current dataframe
+print(df.sample(5)) # Display random 5 records
+
+print(df.info())
