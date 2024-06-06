25e78483031cfb712faae0d8a676bb7e1ed36fda
JulianLoewenherz <juliancollege123@gmail.com>
2024-06-06 16:33:26 -0700

    updated

--- linearRegression.py
+++ linearRegression.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("Fishnums.csv")
+print('Shape of dataset= ', df.shape) # To get no of rows and columns
+df.head(5) # head(n) returns first n records only. Can also use sample(n) for random n records.
